8e0bbf4d76ed76dc21bbfbc7d5e0ca6613ff465d
Youcef <youcefcopy@gmail.com>
2026-02-15 13:14:28 +0100

    header component has been styled for now

--- src/components/DisplaySection.vue
+++ src/components/DisplaySection.vue
@@ -1,25 +1,55 @@
 <script setup></script>
 
 <template>
-  <h1>Expense Tracker</h1>
-  <div class="balance">
-    <p>Your Ballance:</p>
-  </div>
-  <div class="values">
-    <div class="income">Income</div>
-    <div class="expense">Expense</div>
+  <div class="header">
+    <h3>Expense Tracker</h3>
+    <div class="balance">
+      <p>Your Ballance: <span> $300.99</span></p>
+    </div>
+    <div class="values">
+      <div class="income">
+        Income
+        <span>+$100</span>
+      </div>
+      <div class="expense">
+        Expense
+        <span>-$200</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+h3 {
+  margin: 1rem 0;
+}
+
+span {
+  font-size: 1.3rem;
+  font-weight: 600;
+  display: block;
+  margin: 0.5rem;
+}
 .values {
   display: inline-flex;
-  width: 20%;
+  width: 100%;
+  /* margin: 0 auto; */
+  /* justify-self: center; */
 }
 
 .income,
 .expense {
   background-color: seagreen;
   padding: 0.5rem 1.5rem;
+
+  box-shadow: 2px 2px 2px #000;
+}
+
+.income span {
+  color: green;
+}
+
+.expense span {
+  color: red;
 }
 </style>
